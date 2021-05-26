589d5bb58fd075e9ba5e5acd6db9c44d218af54f
타코여미 <takoyummy@naver.com>
2021-05-26 21:48:13 +0900

    Create NoticeListController.java

---
+++ src/com/stylecast/notice/controller/NoticeListController.java
@@ -0,0 +1,50 @@
+package com.stylecast.notice.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.stylecast.notice.model.service.NoticeService;
+import com.stylecast.notice.model.vo.Notice;
+
+/**
+ * Servlet implementation class NoticeListController
+ */
+@WebServlet("/list.no")
+public class NoticeListController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NoticeListController() {
+        super();
+        
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		ArrayList<Notice> list = new NoticeService().selectNoticeList();
+		
+		request.setAttribute("list", list);
+		request.getRequestDispatcher("views/notice/noticeMain.jsp");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
