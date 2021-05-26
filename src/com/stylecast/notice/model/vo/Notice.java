d49ee212350993968bcc967aa6b2ec95c26e589b
타코여미 <takoyummy@naver.com>
2021-05-26 21:48:22 +0900

    Update Notice.java

--- src/com/stylecast/notice/model/vo/Notice.java
+++ src/com/stylecast/notice/model/vo/Notice.java
@@ -5,7 +5,7 @@ import java.sql.Date;
 public class Notice {
 	
 	private int noticeNo;
-	private int memNo;
+	private String memName;
 	private String noticeTitle;
 	private String noticeContent;
 	private int count;
@@ -16,81 +16,97 @@ public class Notice {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-	public Notice(int noticeNo, int memNo, String noticeTitle, String noticeContent, int count, Date enrDate,
+	
+	public Notice(int noticeNo, String memName, String noticeTitle, String noticeContent, int count, Date enrDate,
 			String status) {
 		super();
 		this.noticeNo = noticeNo;
-		this.memNo = memNo;
+		this.memName = memName;
 		this.noticeTitle = noticeTitle;
 		this.noticeContent = noticeContent;
 		this.count = count;
 		this.enrDate = enrDate;
 		this.status = status;
+	}
+	
+	
+	
+	public Notice(int noticeNo, String memName, String noticeTitle, Date enrDate) {
+		super();
+		this.noticeNo = noticeNo;
+		this.memName = memName;
+		this.noticeTitle = noticeTitle;
+		this.enrDate = enrDate;
 	}
 
 	public int getNoticeNo() {
 		return noticeNo;
 	}
-
+	
 	public void setNoticeNo(int noticeNo) {
 		this.noticeNo = noticeNo;
 	}
-
-	public int getMemNo() {
-		return memNo;
+	
+	public String getMemName() {
+		return memName;
 	}
-
-	public void setMemNo(int memNo) {
-		this.memNo = memNo;
+	
+	public void setMemName(String memName) {
+		this.memName = memName;
 	}
-
+	
 	public String getNoticeTitle() {
 		return noticeTitle;
 	}
-
+	
 	public void setNoticeTitle(String noticeTitle) {
 		this.noticeTitle = noticeTitle;
 	}
-
+	
 	public String getNoticeContent() {
 		return noticeContent;
 	}
-
+	
 	public void setNoticeContent(String noticeContent) {
 		this.noticeContent = noticeContent;
 	}
-
+	
 	public int getCount() {
 		return count;
 	}
-
+	
 	public void setCount(int count) {
 		this.count = count;
 	}
-
+	
 	public Date getEnrDate() {
 		return enrDate;
 	}
-
+	
 	public void setEnrDate(Date enrDate) {
 		this.enrDate = enrDate;
 	}
-
+	
 	public String getStatus() {
 		return status;
 	}
-
+	
 	public void setStatus(String status) {
 		this.status = status;
 	}
-
+	
 	@Override
 	public String toString() {
-		return "Notice [noticeNo=" + noticeNo + ", memNo=" + memNo + ", noticeTitle=" + noticeTitle + ", noticeContent="
-				+ noticeContent + ", count=" + count + ", enrDate=" + enrDate + ", status=" + status + "]";
+		return "Notice [noticeNo=" + noticeNo + ", memName=" + memName + ", noticeTitle=" + noticeTitle
+				+ ", noticeContent=" + noticeContent + ", count=" + count + ", enrDate=" + enrDate + ", status="
+				+ status + "]";
 	}
-
+	
+	
+	
+	
+	
+	
 	
 	
 	
