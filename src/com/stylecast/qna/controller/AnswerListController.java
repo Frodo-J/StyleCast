1a8364f84b528c185818f23955070bfc4541886e
타코여미 <takoyummy@naver.com>
2021-06-01 22:13:23 +0900

    Feat : 댓글 조회 기능 추가

---
+++ src/com/stylecast/qna/controller/AnswerListController.java
@@ -0,0 +1,52 @@
+package com.stylecast.qna.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.stylecast.qna.model.service.QnaService;
+import com.stylecast.qna.model.vo.Qna;
+
+/**
+ * Servlet implementation class AnswerListController
+ */
+@WebServlet("/alist.qna")
+public class AnswerListController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AnswerListController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int qnaNo = Integer.parseInt(request.getParameter("qno"));
+		
+		Qna qAnswer = new QnaService().selectQnaAnswer(qnaNo);
+		System.out.println("answerlistController : " + qAnswer);
+		response.setContentType("application/json; charset=utf-8");
+		new Gson().toJson(qAnswer, response.getWriter());
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
